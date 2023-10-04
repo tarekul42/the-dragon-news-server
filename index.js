3fc656bf3db60c28edfeb0e8874f2be73e8aabd3
tarekul42 <tarekulrifat205@gmail.com>
2023-10-04 01:38:18 -0700

    news json file added and done for this project

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors())
 
@@ -12,7 +13,28 @@ app.get('/', (req, res) =>{
 });
 
 app.get('/categories', (req, res) =>{
-    res.send(categories)
+    res.send(categories);
+});
+
+app.get('/news', (reg, res) =>{
+    res.send(news);
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+});
+
+app.get('/categories/:id', (req, res) =>{
+    const id = req.params.id;
+    if(id == 0){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews)
+    }
 })
 
 app.listen(port, ()=>{
